c83b61be4dbf4c9f17d117c12e76c43025358d1f
Sanch <santi2005531@gmail.com>
2024-10-30 18:56:19 -0300

    Entidades

---
+++ Entidades/Cliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Cliente
+    {
+        #region ATRIBUTOS
+        public string dni;
+        public string nombre;
+        public string direccion;
+        private string contrasena;
+        #endregion
+
+        #region CONSTRUCTOR
+        public Cliente()
+        {
+            dni = string.Empty;
+            nombre = string.Empty;
+            direccion = string.Empty;
+            contrasena = string.Empty;
+        }
+        #endregion
+
+        #region PROPIEDADES/ENCAPSULAMIENTO
+
+        public string IdUsuario
+        {
+            get { return dni; }
+            set { dni = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Direccion
+        {
+            get { return direccion; }
+            set { direccion = value; }
+        }
+
+        public string Contrasena
+        {
+            get { return contrasena; }
+            set { contrasena = value; }
+        }
+
+        #endregion
+    }
+}
---
+++ Entidades/Producto.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Producto
+    {
+        #region ATRIBUTOS
+        private int id;
+        private string nombre;
+        private string marca;
+        private string categoria;
+        private decimal precio;
+        private int stock;
+        #endregion
+
+        #region CONSTRUCTOR
+        public Producto()
+        {
+            nombre = string.Empty;
+            marca = string.Empty;
+            categoria = string.Empty;
+            precio = 0;
+            stock = 0;
+        }
+        #endregion
+
+        #region PROPIEDADES/ENCAPSULAMIENTO
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Categoria
+        {
+            get { return categoria; }
+            set { categoria = value; }
+        }
+
+        public decimal Precio
+        {
+            get { return precio; }
+            set { precio = value; }
+        }
+
+        public int Stock
+        {
+            get { return stock; }
+            set { stock = value; }
+        }
+
+        public int IdProd
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        #endregion
+    }
+}
